b21b5b8ac07df050d9d8e3f1b5bb9206a71197fc
chelxis <michel_loureiro@outlook.com>
2025-11-06 04:03:21 +0000

    Modificado index da página inicial para testes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Oi Myla, ou Kamy, por que to nostálgico, você vai passar em Física 3!!</h1>
+    return <h1>Oi Myla, ou Kamy (por que to nostálgico), você vai passar em Física 3, acredite em você mesma! (mas reserve um tempinho a mais pra estudar) 📐e = ∑∞ⁿ⁼⁰ ¹ₙ 🤓 </h1>
 }
 
 export default Home
