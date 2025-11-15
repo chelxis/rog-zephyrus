c9fe696bf5a535fe20cba732de23e0b2d005dded
chelxis <michel_loureiro@outlook.com>
2025-11-15 23:25:03 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rog-zephyrus
+
 Ajudei minha 'namorada' a comprar um Rog Zephyrus de um dev, batemos um papo sobre carreira, me senti um idiota, tomei vergonha na cara e decidi iniciar o https://curso.dev direito
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home () {
-    return <h1>Oi Myla, ou Kamy (por que to nostálgico), você vai passar em Física 3, acredite em você mesma! (mas reserve um tempinho a mais pra estudar) 📐e = ∑∞ⁿ⁼⁰ ¹ₙ 🤓 </h1>
+function Home() {
+  return (
+    <h1>
+      Oi Myla, ou Kamy (por que to nostálgico), você vai passar em Física 3,
+      acredite em você mesma! (mas reserve um tempinho a mais pra estudar) 📐e =
+      ∑∞ⁿ⁼⁰ ¹ₙ 🤓{" "}
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
